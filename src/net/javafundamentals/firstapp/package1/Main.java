b7c1716c6985d9d375aafc5ca8e01284b02013f5
unknown <abdelfadeelhamdy0@gmail.com>
2024-10-16 15:09:36 +0300

    Add IDE configuration

--- src/net/javafundamentals/firstapp/package1/Main.java
+++ src/net/javafundamentals/firstapp/package1/Main.java
@@ -1,19 +1,25 @@
+package net.javafundamentals.firstapp.package1;
+
+
 public class Main {
     public static void main(String[] args) {
-        int number = 99 ;
+        getNumberInfo(10);
+    }
+
+    public static void getNumberInfo(int number) {
         do {
             int digitsCount = 0;
             int digitsSum = 0;
             int newNumber = 0;
 
-            do{
+            do {
                 int remainder = number % 10;
                 System.out.println(remainder);
                 digitsSum += remainder;
-                newNumber+= remainder * remainder;
+                newNumber += remainder * remainder;
                 number /= 10;
                 digitsCount++;
-            }while (number > 0);
+            } while (number > 0);
 
             System.out.println("Digits Count is " + digitsCount);
             System.out.println("Digits Sum is " + digitsSum);
